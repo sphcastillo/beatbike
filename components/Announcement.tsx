ec5aec53a285bd4ddd30790152dab94656865691
Sophia Castillo <sphcastillo@gmail.com>
2024-04-16 16:45:07 -0700

    Changes to mobile screen nav

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -7,7 +7,7 @@ function Announcement() {
     <div className={mulish.className}>
       <div className='bg-[#333333] py-3'>
         <div className='flex items-center justify-center'>
-            <h1 className='text-white tracking-wider'>NEW CLASSES SCHEDULES EVERY SUNDAY AT 4:30 PM</h1>
+            <h1 className='text-sm px-4 text-white tracking-wider text-center'>NEW CLASSES SCHEDULES EVERY SUNDAY AT 4:30 PM</h1>
         </div>
     </div>
   </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -23,32 +23,16 @@ function Header() {
     return (
             <div>
                 <header className="sticky top-0 z-50">
-                    <nav className="flex items-center justify-between p-6 lg:px-8">
+                    <nav className="flex items-center p-6 lg:px-8">
                         <div className="flex lg:flex-1">
                             <a href="#" className="-m-1.5 p-1.5">
-                            {/* <span className="sr-only">LyricoLingo</span> */}
-                            <div className={comfortaa.className}>
-                            <h2 className="text-white">Beatbike</h2>
-                            </div>
-                            {/* <Image
-                                className="h-[72px] w-auto z-25"
-                                src={LyricoLingoLogo}
-                                alt="LyricoLingo Logo"
-                                width={250}
-                                height={250}
-                            /> */}
+                                <span className="sr-only">Beatbike</span>
+                                <div className={comfortaa.className}>
+                                    <h2 className="text-white">Beatbike</h2>
+                                </div>
                             </a>
                         </div>
-                        <div className="flex lg:hidden">
-                            <button
-                            type="button"
-                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-                            onClick={() => setMobileMenuOpen(true)}
-                            >
-                            <span className="sr-only">Open main menu</span>
-                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-                        </div>
+                        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                         <div className={notoSans.className}>
                             <div className="hidden lg:flex lg:gap-x-12">
                                 {navigation.map((item) => (
@@ -58,33 +42,34 @@ function Header() {
                                 ))}
                             </div>
                         </div>
-                        
-                        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                            <div className={notoSans.className}>
-                                <a href="#" className="text-sm font-semibold leading-6 text-white tracking-wider">
-                                Log in
-                                </a>
-                            </div>
+                        <a href="#" className="text-sm font-semibold leading-6 text-[#DFFF00] tracking-wider ml-6">
+                            Log in
+                        </a>
+                        </div>
+                        <div className="flex lg:hidden justify-end flex-1">
+                            <button
+                                type="button"
+                                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+                                onClick={() => setMobileMenuOpen(true)}
+                            >
+                                <span className="sr-only">Open main menu</span>
+                                <Bars3Icon className="h-6 w-6 text-[#DFFF00]" aria-hidden="true" />
+                            </button>
                         </div>
                     </nav>
 
                     <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                         <div className="fixed inset-0 z-50" />
-                        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+                        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#000000] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
                             <div className="flex items-center justify-between">
-                            {/* <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
-                                <Image
-                                className="h-8 w-auto"
-                                src={LyricoLingoLogo}
-                                alt=""
-                                width={250}
-                                height={250}
-                                />
-                            </a> */}
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <div className={comfortaa.className}>
+                                    <h2 className="text-white">Beatbike</h2>
+                                </div>
+                            </a>
                             <button
                                 type="button"
-                                className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                                className="-m-2.5 rounded-md p-2.5 text-[#DFFF00]"
                                 onClick={() => setMobileMenuOpen(false)}
                             >
                                 <span className="sr-only">Close menu</span>
