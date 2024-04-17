a0052ffeefeb010d6cfc4a1ad1ce4c3d17fe0a0a
Sophia Castillo <sphcastillo@gmail.com>
2024-04-17 10:44:07 -0700

    Vercel  ERROR: URL invalid

--- lib/getUrl.ts
+++ lib/getUrl.ts
@@ -1,7 +1,18 @@
-// Description: This file defines a function that returns the URL of the given path.
-const getUrl = (path: string) =>
-    process.env.NODE_ENV === 'development'
-    ? `http://localhost:3000${path}`
-    : process.env.VERCEL_URL + '/' + path;
+// // Description: This file defines a function that returns the URL of the given path.
+// const getUrl = (path: string) =>
+//     process.env.NODE_ENV === 'development'
+//     ? `http://localhost:3000${path}`
+//     : process.env.VERCEL_URL + '/' + path;
 
-export default getUrl;
+// export default getUrl;
+
+const getUrl = (path: string) => {
+    // Remove leading slash from path if it exists
+    const formattedPath = path.startsWith('/') ? path.slice(1) : path;
+  
+    return process.env.NODE_ENV === 'development'
+      ? `http://localhost:3000/${formattedPath}`
+      : `${process.env.VERCEL_URL}/${formattedPath}`;
+  };
+  
+  export default getUrl;
