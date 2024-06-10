e27fac60e2ef56229516e82432da8098e37cae88
sphcastillo <sphcastillo@gmail.com>
2024-06-10 15:37:05 -0700

    adding of location[s] pages

---
+++ app/huntington-beach/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HuntingtonBeachPage() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default HuntingtonBeachPage;
---
+++ app/tarzana/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TarzanaPage() {
+  return (
+    <div>TarzanaPage</div>
+  )
+}
+
+export default TarzanaPage;
