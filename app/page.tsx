c9ee4047a3e5a798d9f458b38bf0b04b0551bb2b
sphcastillo <sphcastillo@gmail.com>
2024-06-09 18:41:19 -0700

    ERROR: what's wrong check

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,14 @@
-'use client'
 import Hero from "@/components/Hero";
 import Introduction from "@/components/Introduction";
 import Locations from "@/components/Locations";
-import MobileCarousel from "@/components/MobileCarousel";
-import { useEffect, useState } from "react";
 import Staff from "@/components/Staff";
 import Newletter from "@/components/Newsletter";
 import CommunityGallery from "@/components/CommunityGallery";
 
 export default function Home() {
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 640);
-    }
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  }, []);
 
     return (
       <main className="">
-        {/* {isSmallScreen ? <MobileCarousel /> : <Hero />} */}
         <Hero />
         <Introduction />
         <Locations />
