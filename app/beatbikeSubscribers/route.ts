27965f6f473f66878e492e27fe874acbc83c6f93
sphcastillo <sphcastillo@gmail.com>
2024-06-12 08:42:00 -0700

    Route removed ~ using Server Actions for forms

--- app/beatbikeSubscribers/route.ts
+++
@@ -1,8 +0,0 @@
-import { fetchBeatbikeSubscribers } from "@/mongodb/models/BeatbikeSubscriber";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(){
-    const beatbikeSubscribers = await fetchBeatbikeSubscribers();
-
-    return NextResponse.json(beatbikeSubscribers);
-}
