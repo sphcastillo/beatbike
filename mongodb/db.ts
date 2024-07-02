1541db1d5b667b64c17f28935833f2a22e32b98d
sphcastillo <sphcastillo@gmail.com>
2024-07-02 13:24:12 -0700

    Adding to distinguish between database name on MongoDB connectionString

--- mongodb/db.ts
+++ mongodb/db.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
 const dbName = process.env.MONGO_DB_NAME;
+console.log("dbName: ", dbName);
 const collectionName = process.env.MONGO_COLLECTION_NAME;
+console.log("collectionName: ", collectionName);
 
-
-const connectionString = `mongodb+srv://sphcastillo:${process.env.MONGO_DB_PASSWORD}@newslettersubscriber.iyaf5cv.mongodb.net/?retryWrites=true&w=majority&appName=NewsletterSubscriber`
+const connectionString = `mongodb+srv://sphcastillo:${process.env.MONGO_DB_PASSWORD}@newslettersubscriber.iyaf5cv.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=NewsletterSubscriber`
 
 if(!connectionString){
     throw new Error('ATTENTION: Please provide a connection string');
