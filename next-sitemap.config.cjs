b626bc177e610426b71087c8775f71099145391a
sphcastillo <sphcastillo@gmail.com>
2026-02-03 16:12:20 -0500

    Add sitemap configuration: Introduced next-sitemap.config.cjs to define site URL, sitemap settings, and excluded paths for improved SEO management.

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    siteUrl: "https://beatbikecycling.com",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: "weekly",
+    priority: 0.7,
+    exclude: [
+      "/dashboard",
+      "/signin",
+      "/signin/*",
+      "/signup",
+      "/signup/*",
+      "/forgot-password",
+      "/reset-password",
+      "/admin",
+      "/login",
+    ],
+  };
